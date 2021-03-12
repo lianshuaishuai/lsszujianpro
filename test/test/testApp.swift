146482b7a6f77d6a6e32ce3ec9360328e045a5cc
连帅帅 <741312975@qq.com>
2021-03-12 09:07:12 +0800

    测试

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 连帅帅 on 2021/3/12.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
